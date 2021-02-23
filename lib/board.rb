bab7366764e8a5b4ae4469908b8c286c7ed5132c
sjcco <73748496+sjcco@users.noreply.github.com>
2021-02-23 11:50:39 -0600

    add libraries for displaying to console

---
+++ lib/board.rb
@@ -0,0 +1,31 @@
+class Board
+  attr_reader :cells
+
+  def initialize
+    @cells = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+
+  def display_board
+    puts <<-HEREDOC
+
+         #{@cells[0]} | #{@cells[1]} | #{@cells[2]}
+        ---+---+---
+         #{@cells[3]} | #{@cells[4]} | #{@cells[5]}
+        ---+---+---
+         #{@cells[6]} | #{@cells[7]} | #{@cells[8]}
+
+    HEREDOC
+  end
+
+  def update_board(number, player_symbol)
+    @cells[number - 1] = player_symbol
+  end
+
+  def valid_move?(number)
+    @cells[number - 1] == number
+  end
+
+  def full?
+    @cells.all? { |cell| cell =~ /[^0-9]/ }
+  end
+end
