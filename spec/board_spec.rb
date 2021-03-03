b1b18671696a520ae5ce7c2137c1f91efe84e0fd
sjcco <73748496+sjcco@users.noreply.github.com>
2021-03-02 19:09:19 -0600

    Add spec for update board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,4 +7,12 @@ describe Board do
       expect(board.generate_board.is_a?(String)).to eql(true)
     end
   end
+
+  describe '#update_board' do
+    it 'update @cells array' do
+      board = Board.new
+      board.update_board(1, 'G')
+      expect(board.cells[0]).to eql('G')
+    end
+  end
 end
